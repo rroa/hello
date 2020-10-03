df3a91de64ba8ce89b9cc8569709ba54363fac41
CodingWithAmit <72186433+CodingWithAmit-07@users.noreply.github.com>
2020-10-03 14:33:01 +0530

    Update main.cpp

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,10 +1,12 @@
+//Code starts
 #include <iostream>
 #include <string>
 
 int main(int argc, char** argv)
 {
-	int x = 100;
+	int x = 100;   //inishilising x as 100
 	std::string name = "Raul";
 	std::cout << name << "\n";
 	return 0;
 }
+//code ends
